caf5b8701439ff40436186db3f5f43ec4d671156
Joaquin R <48088064+joaquinalbares@users.noreply.github.com>
2021-01-27 14:19:07 +0100

    arreglo problema mensaje final

--- 3. COMUNICACIONES/CODIGO/Server.java
+++ 3. COMUNICACIONES/CODIGO/Server.java
@@ -1,68 +1,69 @@
-import java.net.*; 
-import java.io.*; 
-import java.util.logging.Level; 
-import java.util.logging.Logger; 
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemplotcp;
 
-public class Server 
-{ 
-	//initialize socket and input stream 
-	private Socket		 socket = null; 
-	private ServerSocket miSocket = null; 
-	private DataInputStream in	 = null; 
-	// Create a Logger 
-	Logger logger  	=  null;
+/**
+ *
+ * @author profesor
+ */
+import java.net.*;
+import java.io.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-	// constructor with port 
-	public Server(int port) 
-	{ 
-		// starts server and waits for a connection 
-		try
-		{ 
-			miSocket = new ServerSocket(port); 
-			System.out.println("Server started"); 
+public class Server {
+    //initialize socket and input stream 
 
-			System.out.println("Waiting for a client ..."); 
+    private Socket socket = null;
+    private ServerSocket miSocket = null;
+    private DataInputStream in = null;
+    // Create a Logger 
+    Logger logger = null;
 
-			socket = miSocket.accept(); 
-			System.out.println("Client accepted"); 
+    // constructor with port 
+    public Server(int port) {
+        // starts server and waits for a connection 
+        try {
+            miSocket = new ServerSocket(port);
+            System.out.println("Server started");
 
-			// takes input from the client socket 
-			in = new DataInputStream( 
-				new BufferedInputStream(socket.getInputStream())); 
+            System.out.println("Waiting for a client ...");
 
-			String line = ""; 
+            socket = miSocket.accept();
+            System.out.println("Client accepted");
 
-			// reads message from client until "Over" is sent 
-			
-			while (!line.equals("Over")) 
-			{
-				try
-				{ 
-					
-						line = in.readUTF(); 
-						System.out.println(line); 
-					
-				} 
-				catch(IOException i) 
-				{ 
-					System.out.println(i); 
-				} 
-			}
-			
-				logger.log(Level.INFO,"Closing connection"); 
+            // takes input from the client socket 
+            in = new DataInputStream(
+                    new BufferedInputStream(socket.getInputStream()));
 
-			// close connection 
-			socket.close(); 
-			in.close(); 
-		} 
-		catch(IOException i) 
-		{ 
-			logger.log(Level.SEVERE, i.getMessage(), i);
-		} 
-	} 
+            String line = "";
 
-	public static void main(String[] args) 
-	{ 
-		Server server = new Server(5000); 
-	} 
-} 
+            // reads message from client until "Over" is sent 
+            while (!line.equals("Over")) {
+                try {
+
+                    line = in.readUTF();
+                    System.out.println(line);
+
+                } catch (IOException i) {
+                    System.out.println(i);
+                }
+            }
+
+            System.out.println("Closing connection");
+
+            // close connection 
+            socket.close();
+            in.close();
+        } catch (IOException i) {
+            logger.log(Level.SEVERE, i.getMessage(), i);
+        }
+    }
+
+    public static void main(String[] args) {
+        Server server = new Server(5000);
+    }
+}
